cb6e134fe793555c2c5a726aa83bbd00224ceb49
SPARATA <ahmedsoudani09@gmail.com>
2023-08-02 10:38:47 +0100

    styling the index page

---
+++ commerce/auctions/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    username = models.CharField(max_length=20, unique=True, null=False)
+    email = models.EmailField(unique=True, null=False)
+    password = models.CharField(max_length=128, null=False)
+
+    def __str__(self) :
+        return f"Username:{self.username}, email:{self.email}"
+
+class listing(models.Model):
+    title = models.CharField(max_length=200, blank=False, null=False)
+    description = models.TextField(null=False)
+    price = models.DecimalField(decimal_places=2, max_digits=10, null=False)
+    isactive = models.BooleanField(default=True)
+    image_URL = models.URLField(max_length=200, blank=True, null=True)
+    winner = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return f"Title:{self.title}...Price:{self.price}...Active:{self.isactive}"
